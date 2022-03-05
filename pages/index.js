4bc47abcef74b6c3cd6a22cfd518f605df062f6e
Zytha <yangyong080228@gmail.com>
2022-03-05 12:46:48 +0800

    📜 Added Homepage Embed

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Head from 'next/head';
 
 import Social from '../components/Social';
 import Collab from '../components/Collab';
@@ -12,6 +13,14 @@ import PlaceHolder from '../public/images/invalid.png';
 export default function Home() {
     return (
         <>
+            <Head>
+                <title>Zytha's Homepage</title>
+                <meta name="description" content="Hello there! Welcome to my homepage, I'm Zytha. A passionate front-end web developer from Malaysia. I have experience in developing awesome static websites with TailwindCSS and Next.js."/>
+                <meta property="og:type" content="Homepage"/>
+                <meta property="og:url" content="https://www.zytha.ml"/>
+                <meta name="theme-color" content="#FF0000"/>
+            </Head>
+
             <article>
                 <h1>Homepage</h1>
                 <div className="relative flex py-5 items-center">
