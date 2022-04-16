9aedb59fbb7303881416e13e8bc9b49b7fe736a4
Zytha <yangyong080228@gmail.com>
2022-04-16 23:36:58 +0800

    🧭 Improved Performance

--- src/components/Collab.jsx
+++ src/components/Collab.jsx
@@ -3,15 +3,15 @@ import { motion } from 'framer-motion';
 export default function Collab(props){
     return (
         <>
-          <div className='work-div drop-shadow-2xl'>
-            <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }} className='flex justify-center'>
-              <img src={props.src} className='object-cover rounded-xl aspect-video' alt={props.alt}/>
-            </motion.div>
-            <div>
-              <h2 className='text-center'>{props.title}</h2>
-              <p className='text-center'>{props.content}</p> 
+            <div className='work-div drop-shadow-2xl'>
+                <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }} className='flex justify-center'>
+                    <img src={props.src} width={ 1280 } height={ 720 } alt={props.alt} className='object-cover rounded-xl aspect-video'/>
+                </motion.div>
+                <div>
+                    <h2 className='text-center'>{props.title}</h2>
+                    <p className='text-center'>{props.content}</p> 
+                </div>
             </div>
-          </div>
         </>
     )
 }
